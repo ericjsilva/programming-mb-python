3646672da8171ea644d750b63ceced6d2bdb47da
Eric Silva <eric.silva@pfizer.com>
2015-12-26 09:00:16 -0500

    Added additional skeleton program.

--- lab2/temp_convert_CtoF.py
+++ lab2/temp_convert_CtoF.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""temp_convert.py: Convert temperature F to C."""
+"""temp_convert.py: Convert temperature C to F."""
 
 # initialize looping variable, assume yes as first answer
 continueYN = "Y"
 
 while continueYN.upper() == "Y":
     # get temperature input from the user, and prompt them for what we expect
-    degF = int(raw_input("Enter temperature in degrees Fahrenheit (°F) to convert: "))
+    degF = int(raw_input("Enter temperature in degrees Fahrenheit (°C) to convert: "))
 
-    degC = (degF - 32) * 5/9
+    # TODO: Convert C to F
 
-    print "Temperature in degrees C is: {0}".format(degC)
+    # TODO: Print the results
 
     # check for temperature below freezing...
-    if degC < 0:
-        print "Pack long underwear!"
+    # TODO: Print some helpful information for cold temperatures.
+
 
     # check for it being a very hot day...
-    if degF > 100:
-        print "Remember to hydrate!"
+    # TODO: Print some helpful information for hot temperatures.
 
     continueYN = raw_input("Would you like to enter another (Y/N)? ")
 
