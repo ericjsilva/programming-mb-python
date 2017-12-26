8d49a17fe709c4bbd0280c10cdaefd46d324e76c
Eric Silva <eric.silva@pfizer.com>
2017-12-26 18:27:01 -0500

    Added a TODO for trying an alternative method.

--- lab1/dice_roll.py
+++ lab1/dice_roll.py
@@ -15,6 +15,13 @@ while roll_again == "yes" or roll_again == "y":
     print random.randint(min, max)
     print random.randint(min, max)
 
+    # TODO try changing the number of dice thrown using a loop
+    # number_of_dice = 2
+    # i = 0
+    # while i < number_of_dice:
+    #     print "Dice {d}: {v}".format(d=i+1, v=random.randint(min, max))
+    #     i += 1
+
     roll_again = raw_input("Would you like to roll the dice again?")
 
 print "\nThanks for playing!"
